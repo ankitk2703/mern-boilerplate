1d59d77b3c83a575d99cdd42f3a7fa02ab820d97
ANKIT KUMAR <ankitk2703@gmail.com>
2021-05-01 20:35:38 +0530

    created user signup model using salt

--- server/models/user.js
+++ server/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     trim: true,
+    unique:true,
     required: true,
     lowercase: true,
   },
@@ -34,3 +35,37 @@ const userSchema = new mongoose.Schema({
 
 // virtual
 
+userSchema.virtual('password')
+.set(function(password) {
+    this._password = password
+    this.salt = this.makeSalt()
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function(){
+    return this._password
+})
+
+
+// methods
+
+userSchema.methods = {
+
+    authenticate: function(plainText){
+        return this.encryptPassword(plainText) = this.hashed_password
+    },
+    encryptPassword: function(password) {
+            if(!password) return ''
+            try {
+                return crypto.createHmac('sha1',this.salt)
+                .update(password)
+                .digest('hex')
+            } catch (error) {
+                return ''
+            }
+    },
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + ''
+    }
+}
+
+module.exports = mongoose.model('User',userSchema)
